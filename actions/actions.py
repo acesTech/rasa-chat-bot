5bc716deebe76f2352ba74f2fc007309f406b1ee
MrWevr <90693718+MrWevr@users.noreply.github.com>
2022-07-09 16:38:41 -0400

    shopkeeper

--- actions/actions.py
+++ actions/actions.py
@@ -5,18 +5,16 @@ from rasa_sdk.executor import CollectingDispatcher
 import requests
 import random
 
-
-class ActionGetMonster(Action):
+class get_random_weapon(Action):
    def name(self) -> Text:
-        return "action_get_monster"
+        return "get_random_weapon"
    def run(self, dispatcher, tracker, domain):
-       rating = tracker.get_slot('rating')
        current = requests.get(
-           ('https://www.dnd5eapi.co/api/monsters/?challenge_rating={}').format(rating)).json()
-       random_index = random.randrange(current['count'])
-       random_monster = current['results'][random_index]['index']
-       monster_detail = requests.get(
-           ('https://www.dnd5eapi.co/api/monsters/{}').format(random_monster)).json()
-       response = """Prepare to face the {}.""".format(
-           monster_detail['name'])
-       dispatcher.utter_message(response)
+           ('https://www.dnd5eapi.co/api/equipment-categories/weapon')).json()
+       random_index = random.randrange(len(current['equipment']))
+       random_item = current['equipment'][random_index]['url']
+       print(random_item)
+       item_detail = requests.get(
+           ('https://www.dnd5eapi.co{}').format(random_item)).json()
+       print(item_detail['name'])
+       dispatcher.utter_message(response = "utter_sell", item = item_detail['name'])
